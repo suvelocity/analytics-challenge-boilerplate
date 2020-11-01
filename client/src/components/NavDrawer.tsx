820c58492a0a8ead05ab30831fe054f787e7b906
DrLiamKless <67854560+DrLiamKless@users.noreply.github.com>
2020-11-01 10:32:54 +0200

    basic style for dashboard + npm i google maps + styled components

--- client/src/components/NavDrawer.tsx
+++ client/src/components/NavDrawer.tsx
@@ -17,6 +17,7 @@ import PersonIcon from "@material-ui/icons/Person";
 import LogoutIcon from "@material-ui/icons/ExitToApp";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import { Grid, Avatar, Typography } from "@material-ui/core";
 import { formatAmount } from "../utils/transactionUtils";
 import { AuthMachineContext, AuthMachineEvents } from "../machines/authMachine";
@@ -79,6 +80,19 @@ export const mainListItems = (
         <NotificationsIcon />
       </ListItemIcon>
       <ListItemText primary="Notifications" />
+    </ListItem>
+    <ListItem
+      button
+      // @ts-ignore
+      onClick={() => showTemporaryDrawer && toggleDrawer()}
+      component={RouterLink}
+      to="/admin"
+      data-test="sidenav-admin"
+    >
+      <ListItemIcon>
+        <VpnKeyIcon />
+      </ListItemIcon>
+      <ListItemText primary="Admin" />
     </ListItem>
   </div>
 );
--- client/src/containers/DashBoard.tsx
+++ client/src/containers/DashBoard.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import { Interpreter } from "xstate";
 import { AuthMachineContext, AuthMachineEvents } from "../machines/authMachine";
+import { makeStyles, Paper, Typography, Box } from "@material-ui/core";
+import grey from "@material-ui/core/colors/grey";
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    minHeight: "90vh",
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+  },
+}));
 
 export interface Props {
   authService: Interpreter<AuthMachineContext, any, AuthMachineEvents, any>;
 }
 
+
 const DashBoard: React.FC = () => {
+
+  const classes = useStyles();
+
   return (
     <>
+      <Paper className={classes.paper}>
+      <Typography component="h2" variant="h6" color="primary" gutterBottom>
+        Analytics
+      </Typography>
+      <Box      
+        display="flex"
+        height={600}
+        min-height={600}
+        alignItems="center"
+        justifyContent="center"
+        border={1}
+        borderColor={grey[200]}
+      >
+      </Box>
+      </Paper>
     </>
   );
 };
