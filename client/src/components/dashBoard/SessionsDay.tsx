42278041995ff7cd732d716bab86c745db128210
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-11-03 14:42:23 +0200

    basic chart by day and hour

--- client/src/components/dashBoard/SessionsDay.tsx
+++ client/src/components/dashBoard/SessionsDay.tsx
@@ -1,6 +1,15 @@
 import React,{useEffect, useState} from 'react'
 import axios,{ AxiosResponse } from 'axios'
-
+import {
+    ResponsiveContainer,
+    LineChart,
+    Line,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Legend,
+    Tooltip,
+} from "recharts";
 
 
 const SessionsDay: React.FC = () => {
@@ -10,16 +19,29 @@ const SessionsDay: React.FC = () => {
         (async ()=>{
         try {
             const {data}=await axios.get(`http://localhost:3001/events/by-days/${offset}`);
-            console.log(data);
+            setDataByDay(data)
         } catch (error) {
             
         }
     })()}, [])
 
     return (
-        <div>
-            SessionsDay
-        </div>
+        // <div style={{ width: "100vw", height: "80vh" }}>
+        <div style={{ width: "100%", height: "450px" }}>
+        <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+            data={dataByDay}
+            margin={{ top: 20, right: 80, left: 40, bottom: 0 }}
+        >
+            <Line name="aaaaa" type="monotone" dataKey="count" stroke="red" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="date" />
+            <YAxis  />
+            <Legend verticalAlign="top" height={36} />
+            <Tooltip />
+        </LineChart>
+    </ResponsiveContainer>
+    </div>
     )
 }
 
--- client/src/components/dashBoard/SessionsHoues.tsx
+++ client/src/components/dashBoard/SessionsHoues.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import axios,{ AxiosResponse } from 'axios'
+import {
+    ResponsiveContainer,
+    LineChart,
+    Line,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Legend,
+    Tooltip,
+} from "recharts";
+
 
 const SessionsHoues: React.FC = () => {
+    const [dataByHours, setDataByHours] = useState([]);
+    const [offset, setOffset] = useState(5);
+    useEffect(() => {
+        (async ()=>{
+        try {
+            const {data}=await axios.get(`http://localhost:3001/events/by-hours/${offset}`);
+            setDataByHours(data)
+        } catch (error) {
+            
+        }
+    })()}, [])
     return (
-        <div>
-            SessionsHoues
-        </div>
+        // <div style={{ width: "100vw", height: "80vh" }}>
+        <div style={{ width: "100%", height: "450px" }}>
+        <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+            data={dataByHours}
+            margin={{ top: 20, right: 80, left: 40, bottom: 0 }}
+        >
+            <Line name="aaaaa" type="monotone" dataKey="count" stroke="red" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="hour" />
+            <YAxis  />
+            <Legend verticalAlign="top" height={36} />
+            <Tooltip />
+        </LineChart>
+    </ResponsiveContainer>
+    </div>
     )
 }
 
