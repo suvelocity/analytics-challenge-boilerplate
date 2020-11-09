75715b723ebb691f2edc219133a6c0a3061a3d96
AlonBru <indyalon@gmail.com>
2020-11-09 14:36:36 +0200

    UNMESSUP

---
+++ server/backend/event-routes.ts
@@ -0,0 +1,85 @@
+///<reference path="types.ts" />
+
+import express from "express";
+import { Request, Response } from "express";
+
+// some useful database functions in here:
+import {
+} from "./database";
+import { Event, weeklyRetentionObject } from "../../client/src/models/event";
+import { ensureAuthenticated, validateMiddleware } from "./helpers";
+
+import {
+  shortIdValidation,
+  searchValidation,
+  userFieldsValidator,
+  isUserValidator,
+} from "./validators";
+const router = express.Router();
+
+// Routes
+
+interface Filter {
+  sorting: string;
+  type: string;
+  browser: string;
+  search: string;
+  offset: number;
+}
+
+router.get('/all', (req: Request, res: Response) => {
+  res.send('/all')
+    
+});
+
+router.get('/all-filtered', (req: Request, res: Response) => {
+  res.send('/all-filtered')
+});
+
+router.get('/by-days/:offset', (req: Request, res: Response) => {
+  res.send('/by-days/:offset')
+});
+
+router.get('/by-hours/:offset', (req: Request, res: Response) => {
+  res.send('/by-hours/:offset')
+});
+
+router.get('/today', (req: Request, res: Response) => {
+  res.send('/today')
+});
+
+router.get('/week', (req: Request, res: Response) => {
+  res.send('/week')
+});
+
+router.get('/retention', (req: Request, res: Response) => {
+  const {dayZero} = req.query
+  res.send('/retention')
+});
+router.get('/:eventId',(req : Request, res : Response) => {
+  res.send('/:eventId')
+});
+
+router.post('/', (req: Request, res: Response) => {
+  res.send('/')
+});
+
+router.get('/chart/os/:time',(req: Request, res: Response) => {
+  res.send('/chart/os/:time')
+})
+
+  
+router.get('/chart/pageview/:time',(req: Request, res: Response) => {
+  res.send('/chart/pageview/:time')
+})
+
+router.get('/chart/timeonurl/:time',(req: Request, res: Response) => {
+  res.send('/chart/timeonurl/:time')
+})
+
+router.get('/chart/geolocation/:time',(req: Request, res: Response) => {
+  res.send('/chart/geolocation/:time')
+})
+
+
+export default router;
